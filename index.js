3ac94ac9abee7495f308d00fdb5bc82a4755e699
Bhavyatha <bhavyathashetty@gmail.com>
2021-12-26 13:12:24 +0530

    addding changes

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const cors = require('cors')
 const register = require('./controller/register')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt');
-const Pool =require('pg').Pool;
-const PORT = process.env.PORT||3000;
+const Pool = require('pg').Pool;
+const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 app.use(cors())
 
 const db = new Pool({
-   
+    host: 'https://todo-api-bh.herokuapp.com/',
     database: 'postgresql-perpendicular-47895',
-    
+
 
 });
 
@@ -23,41 +23,38 @@ const db = new Pool({
 // })
 
 
-app.get('/users',(req,res)=>
-{register.getUsers(req,res,db)})
+app.get('/users', (req, res) => { register.getUsers(req, res, db) })
 
-app.post('/register', (req, res) => 
-{ register.handleRegister(req, res, db,bcrypt)}
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) }
 )
 
-app.post('/signin',(req,res)=>
-{register.handleSignin(req,res,db,bcrypt)})
+app.post('/signin', (req, res) => { register.handleSignin(req, res, db, bcrypt) })
 
-app.get('/gettodos',(req,res)=>{
-    register.handleGetTodo(req,res,db)
+app.get('/gettodos', (req, res) => {
+    register.handleGetTodo(req, res, db)
 })
 
-app.post('/todo',(req,res)=>{
-    register.handleAddTodo(req,res,db)
+app.post('/todo', (req, res) => {
+    register.handleAddTodo(req, res, db)
 })
 
-app.put('/edittodo',(req,res)=>{
-    register.handleEditTodo(req,res,db)
+app.put('/edittodo', (req, res) => {
+    register.handleEditTodo(req, res, db)
 })
 
-app.delete('/deletetodo',(req,res)=>{
-    register.handleDeleteTodo(req,res,db)
-})
-  
- 
-
-app.get('/api',(req,res)=>{
-    res.json({message:"hello server!"})
+app.delete('/deletetodo', (req, res) => {
+    register.handleDeleteTodo(req, res, db)
 })
 
 
 
-app.listen(PORT,()=>{
+app.get('/api', (req, res) => {
+    res.json({ message: "hello server!" })
+})
+
+
+
+app.listen(PORT, () => {
     console.log(`server listens to port ${PORT}`)
 });
 
